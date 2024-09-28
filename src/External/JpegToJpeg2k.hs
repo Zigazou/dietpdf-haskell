6c4a2d38539eebaa4d8c8710ce28af9e69761c89
Frédéric BISSON <zigazou@protonmail.com>
2024-09-28 20:23:49 +0200

    Incorrect handling of RGB colorspace by ImageMagick

--- src/External/JpegToJpeg2k.hs
+++ src/External/JpegToJpeg2k.hs
@@ -20,20 +20,35 @@ Converts a JPEG image to a lossy JPEG 2000 image.
 -}
 jpegToJpeg2k :: Int -> BS.ByteString -> FallibleT IO BS.ByteString
 jpegToJpeg2k quality jpegImage = do
-  -- Use ImageMagick to convert the JPEG image to a TIFF image because it can
-  -- keep CMYK colorspace in the process.
-  let components = getJpegComponents jpegImage
-      params = if components == 4 then ["-" , "-negate", "-"]
-                                  else ["-", "-"  ]
+  if getJpegComponents jpegImage == 4
+    then do
+      -- Use ImageMagick to convert the JPEG image to a TIFF image because it
+      -- can keep CMYK colorspace in the process.
+      tiffImage <- externalCommandBuf'' "convert"
+                                        ["-" , "-negate", "-"]
+                                        "jpg"
+                                        "tiff"
+                                        jpegImage
 
-  tiffImage <- externalCommandBuf'' "convert" params "jpg" "tiff" jpegImage
-
-  externalCommandBuf'' "grk_compress"
-                       [ "--in-file", "-"
-                       , "--quality", show quality
-                       , "--out-file", "-"
-                       , "--verbose"
-                       ]
-                       "tiff"
-                       "j2k"
-                       tiffImage
+      externalCommandBuf'' "grk_compress"
+                          [ "--in-file", "-"
+                          , "--quality", show quality
+                          , "--out-file", "-"
+                          , "--verbose"
+                          ]
+                          "tiff"
+                          "j2k"
+                          tiffImage
+    else
+      -- For other types of JPEG (RGB, Grayscale), Grok is able to read them.
+      -- It also avoids using ImageMagick which can change the colorspace due
+      -- to a bug in color space handling before ImageMagick v6.9.13.
+      externalCommandBuf'' "grk_compress"
+                          [ "--in-file", "-"
+                          , "--quality", show quality
+                          , "--out-file", "-"
+                          , "--verbose"
+                          ]
+                          "jpg"
+                          "j2k"
+                          jpegImage
