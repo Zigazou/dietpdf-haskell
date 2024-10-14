63701c4019d31ea4115c5d02ecf28a1a58f1e90a
Frédéric BISSON <zigazou@protonmail.com>
2024-10-14 22:02:35 +0200

    Formatting

--- app/Command/GetObjectByNumber.hs
+++ app/Command/GetObjectByNumber.hs
@@ -43,24 +43,91 @@ pretty level (PDFComment comment) = level %> "%" <> comment <> "\n"
 pretty level (PDFVersion version) = level %> "PDF-" <> version <> "\n"
 pretty level PDFEndOfFile = level %> "%%EOF\n"
 pretty level (PDFNumber number) = level %> fromNumber number <> "\n"
-pretty level keyword@(PDFKeyword _keyword) = level %> fromPDFObject keyword <> "\n"
+pretty level keyword@(PDFKeyword _keyword) =
+  level %> fromPDFObject keyword <> "\n"
 pretty level name@(PDFName _name) = level %> fromPDFObject name <> "\n"
-pretty level pdfString@(PDFString _pdfString) = level %> fromPDFObject pdfString <> "\n"
-pretty level hexString@(PDFHexString _hexString) = level %> fromPDFObject hexString <> ">\n"
-pretty level reference@(PDFReference _major _minor) = level %> fromPDFObject reference <> "\n"
-pretty level (PDFArray array) = level %> "[\n" <> BS.concat (pretty (inc level) <$> toList array) <> disp level %> "]\n"
-pretty level (PDFDictionary dict) = level %> "<<\n" <> BS.concat (map (\(key, value) -> inc level %> "/" <> key <> " " <> pretty (hide (inc level)) value) (Map.toList dict)) <> disp level %> ">>\n"
-pretty level (PDFIndirectObject major minor object) = level %> fromInt major <> " " <> fromInt minor <> " obj\n" <> pretty (inc level) object <> level %> "endobj\n"
-pretty level (PDFIndirectObjectWithStream major minor dict _stream) = level %> fromInt major <> " " <> fromInt minor <> " obj\n" <> pretty (inc level) (PDFDictionary dict) <> level %> "stream ... endstream\n" <> level %> "endobj\n"
-pretty level (PDFIndirectObjectWithGraphics major minor dict _gfx) = level %> fromInt major <> " " <> fromInt minor <> " obj\n" <> pretty (inc level) (PDFDictionary dict) <> level %> "stream ... endstream\n" <> level %> "endobj\n"
-pretty level (PDFObjectStream major minor dict _stream) = level %> fromInt major <> " " <> fromInt minor <> " obj\n" <> pretty (inc level) (PDFDictionary dict) <> level %> "stream ... endstream\n" <> level %> "endobj\n"
-pretty level (PDFXRefStream major minor dict _stream) = level %> fromInt major <> " " <> fromInt minor <> " obj\n" <> pretty (inc level) (PDFDictionary dict) <> level %> "stream ... endstream\n" <> level %> "endobj\n"
+pretty level pdfString@(PDFString _pdfString) =
+  level %> fromPDFObject pdfString <> "\n"
+pretty level hexString@(PDFHexString _hexString) =
+  level %> fromPDFObject hexString <> ">\n"
+pretty level reference@(PDFReference _major _minor) =
+  level %> fromPDFObject reference <> "\n"
+pretty level (PDFArray array) =
+     level
+  %> "[\n" <> BS.concat (pretty (inc level) <$> toList array)
+  <> disp level %> "]\n"
+pretty level (PDFDictionary dict) =
+     level
+  %> "<<\n"
+  <> BS.concat (map (\(key, value) -> inc level
+                                   %> "/"
+                                   <> key
+                                   <> " "
+                                   <> pretty (hide (inc level)) value)
+                                   (Map.toList dict))
+  <> disp level
+  %> ">>\n"
+pretty level (PDFIndirectObject major minor object) =
+     level
+  %> fromInt major
+  <> " "
+  <> fromInt minor
+  <> " obj\n"
+  <> pretty (inc level) object
+  <> level
+  %> "endobj\n"
+pretty level (PDFIndirectObjectWithStream major minor dict _stream) =
+     level
+  %> fromInt major
+  <> " "
+  <> fromInt minor
+  <> " obj\n"
+  <> pretty (inc level) (PDFDictionary dict)
+  <> level
+  %> "stream ... endstream\n"
+  <> level
+  %> "endobj\n"
+pretty level (PDFIndirectObjectWithGraphics major minor dict _gfx) =
+     level
+  %> fromInt major
+  <> " "
+  <> fromInt minor
+  <> " obj\n"
+  <> pretty (inc level) (PDFDictionary dict)
+  <> level
+  %> "stream ... endstream\n"
+  <> level
+  %> "endobj\n"
+pretty level (PDFObjectStream major minor dict _stream) =
+     level
+  %> fromInt major
+  <> " "
+  <> fromInt minor
+  <> " obj\n"
+  <> pretty (inc level) (PDFDictionary dict)
+  <> level
+  %> "stream ... endstream\n"
+  <> level
+  %> "endobj\n"
+pretty level (PDFXRefStream major minor dict _stream) =
+     level
+  %> fromInt major
+  <> " "
+  <> fromInt minor
+  <> " obj\n"
+  <> pretty (inc level) (PDFDictionary dict)
+  <> level
+  %> "stream ... endstream\n"
+  <> level
+  %> "endobj\n"
 pretty level (PDFBool True) = level %> "true\n"
 pretty level (PDFBool False) = level %> "false\n"
 pretty level PDFNull = level %> "null\n"
 pretty level (PDFXRef _subsections) = level %> "xref\n" <> "...\n"
-pretty level (PDFTrailer trailer) = level %> "trailer\n" <> pretty (inc level) trailer
-pretty level (PDFStartXRef start) = level %> "startxref\n" <> fromInt start <> "\n"
+pretty level (PDFTrailer trailer) =
+  level %> "trailer\n" <> pretty (inc level) trailer
+pretty level (PDFStartXRef start) =
+  level %> "startxref\n" <> fromInt start <> "\n"
 
 getObjectByNumber :: Int -> PDFDocument -> FallibleT IO ()
 getObjectByNumber objectNumber objects = do
