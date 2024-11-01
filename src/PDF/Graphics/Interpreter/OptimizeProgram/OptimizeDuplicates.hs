e73b2caf12b2052ce5f607999ee0d62d85e542bb
Frédéric BISSON <zigazou@protonmail.com>
2024-11-01 11:23:39 +0100

    Handling of types of color operator (stroke vs non-stroke)

--- src/PDF/Graphics/Interpreter/OptimizeProgram/OptimizeDuplicates.hs
+++ src/PDF/Graphics/Interpreter/OptimizeProgram/OptimizeDuplicates.hs
@@ -4,8 +4,8 @@ module PDF.Graphics.Interpreter.OptimizeProgram.OptimizeDuplicates
 
 import Data.PDF.Command (Command (Command))
 import Data.PDF.GFXObject
-    ( GSOperator (GSSetColourRenderingIntent, GSSetFlatnessTolerance, GSSetLineCap, GSSetLineDashPattern, GSSetLineJoin, GSSetLineWidth, GSSetMiterLimit, GSSetNonStrokeCMYKColorspace, GSSetNonStrokeColor, GSSetNonStrokeColorN, GSSetNonStrokeColorspace, GSSetNonStrokeGrayColorspace, GSSetNonStrokeRGBColorspace, GSSetParameters, GSSetStrokeCMYKColorspace, GSSetStrokeColor, GSSetStrokeColorN, GSSetStrokeColorspace, GSSetStrokeGrayColorspace, GSSetStrokeRGBColorspace)
-    )
+  ( GSOperator (GSSetColourRenderingIntent, GSSetFlatnessTolerance, GSSetLineCap, GSSetLineDashPattern, GSSetLineJoin, GSSetLineWidth, GSSetMiterLimit, GSSetNonStrokeCMYKColorspace, GSSetNonStrokeColor, GSSetNonStrokeColorN, GSSetNonStrokeColorspace, GSSetNonStrokeGrayColorspace, GSSetNonStrokeRGBColorspace, GSSetParameters, GSSetStrokeCMYKColorspace, GSSetStrokeColor, GSSetStrokeColorN, GSSetStrokeColorspace, GSSetStrokeGrayColorspace, GSSetStrokeRGBColorspace)
+  )
 import Data.PDF.Program (Program)
 import Data.Sequence (Seq (Empty, (:<|)), (<|))
 
@@ -32,6 +32,22 @@ uselessWhenDuplicated GSSetColourRenderingIntent   = True
 uselessWhenDuplicated GSSetFlatnessTolerance       = True
 uselessWhenDuplicated _anyOtherOperator            = False
 
+isStrokeColorOperator :: GSOperator -> Bool
+isStrokeColorOperator GSSetStrokeColor          = True
+isStrokeColorOperator GSSetStrokeColorN         = True
+isStrokeColorOperator GSSetStrokeRGBColorspace  = True
+isStrokeColorOperator GSSetStrokeCMYKColorspace = True
+isStrokeColorOperator GSSetStrokeGrayColorspace = True
+isStrokeColorOperator _anyOtherOperator         = False
+
+isNonStrokeColorOperator :: GSOperator -> Bool
+isNonStrokeColorOperator GSSetNonStrokeColor          = True
+isNonStrokeColorOperator GSSetNonStrokeColorN         = True
+isNonStrokeColorOperator GSSetNonStrokeRGBColorspace  = True
+isNonStrokeColorOperator GSSetNonStrokeCMYKColorspace = True
+isNonStrokeColorOperator GSSetNonStrokeGrayColorspace = True
+isNonStrokeColorOperator _anyOtherOperator            = False
+
 {- |
 Remove duplicated consecutive operators.
 -}
@@ -40,7 +56,11 @@ optimizeDuplicates Empty = mempty
 optimizeDuplicates (command1@(Command operator1 _anyParameters1)
                 :<| command2@(Command operator2 _anyParameters2)
                 :<| rest)
-  | operator1 /= operator2 = command1 <| optimizeDuplicates (command2 <| rest)
-  | uselessWhenDuplicated operator1 = optimizeDuplicates (command2 <| rest)
+  | operator1 == operator2 && uselessWhenDuplicated operator1
+    = optimizeDuplicates (command2 <| rest)
+  | isStrokeColorOperator operator1 && isStrokeColorOperator operator2
+    = optimizeDuplicates (command2 <| rest)
+  | isNonStrokeColorOperator operator1 && isNonStrokeColorOperator operator2
+    = optimizeDuplicates (command2 <| rest)
   | otherwise = command1 <| optimizeDuplicates (command2 <| rest)
 optimizeDuplicates (command :<| rest) = command <| optimizeDuplicates rest
